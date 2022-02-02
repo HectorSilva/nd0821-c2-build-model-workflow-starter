e03cb8204afd2e2e86d039c58d2181a56070bbc8
Fernando Silva <hector.fernando.silva@assurant.com>
2022-02-01 22:36:31 -0500

    Improve boundaries

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -32,6 +32,10 @@ def go(args):
     # Convert last_review to datetime
     df['last_review'] = pd.to_datetime(df['last_review'])
 
+    idx = df['longitude'].between(-74.25, -
+                                  73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
+
     df.to_csv("clean_sample.csv", index=False)
 
     artifact = wandb.Artifact(
@@ -39,10 +43,6 @@ def go(args):
         type=args.output_type,
         description=args.output_description,
     )
-
-    idx = df['longitude'].between(-74.25, -
-                                  73.50) & df['latitude'].between(40.5, 41.2)
-    df = df[idx].copy()
 
     artifact.add_file("clean_sample.csv")
     run.log_artifact(artifact)
